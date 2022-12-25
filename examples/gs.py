55c5720f2e894a88d83757d5297bb3e0cd3878eb
Sofia N. Lima <me@dzshn.xyz>
2022-12-25 20:09:52 -0300

    "golfscript", @Vap0r1ze answered, unknowingly

---
+++ examples/gs.py
@@ -0,0 +1,20 @@
+##define coding=pypp
+
+# very real golfscript implementation
+# <http://golfscipt.com/golfscipt>
+
+c = [
+!define , lambda: a[:-1] + [len(a[-1])],
+!define . lambda: a + [a[-1]],
+!define @ lambda: a[:-3] + [a[-2], a[-1], a[-3]],
+!define p lambda: (print(a[-1]), a[:-1])[-1],
+!define + lambda: a[:-2] + [a[-1] + a[-2]],
+!define ; lambda: a[:-1],
+!define * lambda: (globals().__setitem__("c", a[-1] * a[-2] + c), a[:-2])[-1],
+!define 1 1,
+!define 30 30,
+!define { [
+!define } ],
+!define ?ENDMARKER ];a=[[]]?NEWLINE while c: x=c.pop(0); a = x() if callable(x) else a + [x]?ENDMARKER
+
+,1{.@.p@+}30*;
